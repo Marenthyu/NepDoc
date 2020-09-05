6874323c111f502d45f44ec8eec6c630c8facef3
Marenthyu <petersilie93@live.de>
2020-09-05 13:13:03 +0200

    Add quickstart and adminref to repo

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ./ts.sh
 ruby nav.rb
-rm -rf ./deploy/{fonts,godsound.ogg,main.css,nepdoc.html}
-cp -r {fonts,godsound.ogg,main.css,nepdoc.html} ./deploy/
+rm -rf ./deploy/{quickstart,adminref.html,fonts,godsound.ogg,main.css,nepdoc.html}
+cp -r {quickstart,adminref.html,fonts,godsound.ogg,main.css,nepdoc.html} ./deploy/
